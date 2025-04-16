c75884fcbe41af35558c5ec5df1e9c8895d8d2c9
Christian Arroyo <chris.cap22@gmail.com>
2025-04-16 16:02:46 -0700

    committing some csharp test data

--- csharp/test/obj/Debug/net9.0/test.AssemblyInfo.cs
+++ csharp/test/obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed8786427d9e31aab3a3d7f2b65e192bf459b49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda2b33037b7936fad02b3f93743c66ec6a4f537")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
